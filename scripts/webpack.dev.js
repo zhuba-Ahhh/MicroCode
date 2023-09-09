b111e925d6a55c3c3c04f907e6a137dc3ac5319a
zhuba-Ahhh <3477826311@qq.com>
2023-09-09 17:34:31 +0800

    refactor: :recycle: index change to build

--- scripts/webpack.dev.js
+++ scripts/webpack.dev.js
@@ -7,6 +7,7 @@ module.exports = Object.assign(
     entry: {
       ["page-pc"]: path.resolve(__dirname, `../src/page-pc/index.tsx`),
       ["preview"]: path.resolve(__dirname, `../src/preview.tsx`),
+      ["main"]: path.resolve(__dirname, `../src/main/main.tsx`),
     },
   },
   commonCfg
---
+++ src/main/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Index from "./components/Index";
+import "./components/index.less";
+
+const App: React.FC = () => {
+  return (
+    <>
+      <Index />
+    </>
+  );
+};
+
+export default App;
