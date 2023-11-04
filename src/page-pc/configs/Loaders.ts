32551443bacc7850f485d487f3a4086645cf2c84
zhuba-Ahhh <3477826311@qq.com>
2023-11-04 10:38:18 +0800

    chore: :hammer: update designer

--- src/page-pc/configs/Loaders.ts
+++ src/page-pc/configs/Loaders.ts
@@ -4,9 +4,9 @@ export default (projectJson: string) => ({
     return new Promise<string[]>((resolve) => {
       resolve([
         // PC通用组件库（提供了包括表单容器、表格、卡片等等常用的组件）
-        `https://f2.eckwai.com/kos/nlav12333/fangzhou/pub/comlibs/7632_1.3.77/2023-10-27_11-56-51/edit.js`,
+        `https://f2.eckwai.com/kos/nlav12333/fangzhou/pub/comlibs/7632_1.3.90/2023-11-03_17-13-02/edit.js`,
         // 基础组件库（提供了基础的组件，如：文本、形状、JS计算、类型转换等）
-        `https://f2.eckwai.com/kos/nlav12333/fangzhou/pub/comlibs/7182_1.0.75/2023-10-12_10-48-27/edit.js`
+        `https://f2.eckwai.com/kos/nlav12333/fangzhou/pub/comlibs/7182_1.0.79/2023-11-03_11-41-19/edit.js`
         // 图表组件库
         // `https://f2.eckwai.com/kos/nlav12333/fangzhou/pub/comlibs/1047_1.0.10/2023-08-31_21-24-51/edit.js`,
         // 拓展组件库
