69dc7f474383ba253527c6c9a90958baae87afae
zhuba-Ahhh <3477826311@qq.com>
2023-12-13 11:41:28 +0800

    perf: :zap: tips

--- src/page-pc/components/Toolbar/Tips/index.tsx
+++ src/page-pc/components/Toolbar/Tips/index.tsx
@@ -1,105 +1,36 @@
-import React, { useState } from 'react';
+import React, { FC, memo, useState } from 'react';
 import { Popover, Modal, Divider } from 'antd';
 import css from './index.less';
 import { keyIcon } from './Icons';
 import { uuid } from '../../../../tools';
-import { infoList, optList } from './constants';
+import { infoList, optList, Opt } from './constants';
 
 interface TipsProps {
   opt?: Opt[];
   info?: Opt[];
 }
 
-interface Opt {
-  name?: string;
-  keys?: any[];
-}
-
-export const Tips = ({ opt = optList, info = infoList }: TipsProps) => {
+const Tips: FC<TipsProps> = ({ opt = optList, info = infoList }: TipsProps) => {
   const [isInfoModalOpen, setIsInfoModalOpen] = useState(false);
 
-  const infoListRender = (list: any[]) => {
+  const listItemRender = (list: Opt[], itemStyle: string) => {
     return (
-      <div className={css.infoItem}>
-        {list.map((item) => {
-          return (
+      <div className={itemStyle === 'info' ? css.infoItem : css.optItem}>
+        {list?.length > 0 &&
+          list.map((item) => (
             <div className={css.itemList} key={uuid()}>
               <div className={css.itemListLeft}>{item.name}</div>
               <div className={css.itemListRight}>
-                {item.keys.map(
-                  (
-                    key:
-                      | string
-                      | number
-                      | boolean
-                      | React.ReactElement<any, string | React.JSXElementConstructor<any>>
-                      | Iterable<React.ReactNode>
-                      | null
-                      | undefined
-                  ) => {
-                    if (key !== '/') {
-                      return (
-                        <div className={css.liBtn} key={uuid()}>
-                          {key}
-                        </div>
-                      );
-                    } else {
-                      return <div key={uuid()}>{key}</div>;
-                    }
-                  }
-                )}
+                {item?.keys &&
+                  item.keys.length > 0 &&
+                  item.keys.map((key) => (
+                    <div className={itemStyle === 'info' ? css.liBtn : css.liOpt} key={uuid()}>
+                      {key}
+                    </div>
+                  ))}
               </div>
             </div>
-          );
-        })}
-      </div>
-    );
-  };
-
-  const optListRender = (list: any[]) => {
-    return (
-      <div className={css.optItem}>
-        {list.map(
-          (item: {
-            name:
-              | string
-              | number
-              | boolean
-              | React.ReactElement<any, string | React.JSXElementConstructor<any>>
-              | Iterable<React.ReactNode>
-              | React.ReactPortal
-              | null
-              | undefined;
-            keys: any[];
-          }) => {
-            return (
-              <div className={css.itemList} key={uuid()}>
-                <div className={css.itemListLeft}>{item.name}</div>
-                <div className={css.itemListRight}>
-                  {item.keys.map(
-                    (
-                      key:
-                        | string
-                        | number
-                        | boolean
-                        | React.ReactElement<any, string | React.JSXElementConstructor<any>>
-                        | Iterable<React.ReactNode>
-                        | React.ReactPortal
-                        | null
-                        | undefined
-                    ) => {
-                      return (
-                        <div className={css.liOpt} key={uuid()}>
-                          {key}
-                        </div>
-                      );
-                    }
-                  )}
-                </div>
-              </div>
-            );
-          }
-        )}
+          ))}
       </div>
     );
   };
@@ -107,38 +38,31 @@ export const Tips = ({ opt = optList, info = infoList }: TipsProps) => {
   return (
     <div>
       <div className={css.tipsPosition}>
-        <div
-          onClick={() => {
-            setIsInfoModalOpen(true);
-          }}
-          className={css.item}
+        <Popover
+          placement="bottom"
+          overlayClassName={css.overlayFilePopover}
+          content={<div className={css.fileInfo}>快捷键和常规操作</div>}
         >
-          <Popover
-            placement="bottom"
-            overlayClassName={css.overlayFilePopover}
-            content={() => {
-              return <div className={css.fileInfo}>快捷键和常规操作</div>;
-            }}
-          >
+          <div onClick={() => setIsInfoModalOpen(true)} className={css.item}>
             <div className={css.filePosition}>{keyIcon}</div>
-          </Popover>
-        </div>
+          </div>
+        </Popover>
       </div>
-
-      {/* 快捷键 */}
       <Modal
         visible={isInfoModalOpen}
-        title={'快捷键和常规操作'}
+        title="快捷键和常规操作"
         footer={null}
         onCancel={() => setIsInfoModalOpen(false)}
         width={1060}
       >
         <div className={css.itemContent}>
-          {infoListRender(info)}
+          {listItemRender(info, 'info')}
           <Divider type="vertical" style={{ height: 'inherit' }} />
-          {optListRender(opt)}
+          {listItemRender(opt, 'opt')}
         </div>
       </Modal>
     </div>
   );
 };
+
+export default memo(Tips);
